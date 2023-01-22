be9811257974f4e5fdc912285756153196a6acef
qwerty3345 <qwerty3345@naver.com>
2023-01-22 23:36:29 +0900

    design: Onboarding 초기 틀 디자인

---
+++ Bankey/Onboarding/OnboardingContainerViewController.swift
@@ -0,0 +1,119 @@
+//
+//  OnboardingContainerViewController.swift
+//  Bankey
+//
+//  Created by Mason Kim on 2023/01/22.
+//
+
+import UIKit
+
+class OnboardingContainerViewController: UIViewController {
+
+    let pageViewController: UIPageViewController
+    var pages = [UIViewController]()
+    var currentVC: UIViewController {
+        didSet {
+        }
+    }
+
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        self.pageViewController = UIPageViewController(transitionStyle: .scroll, navigationOrientation: .horizontal)
+
+        let page1 = ViewController1()
+        let page2 = ViewController2()
+        let page3 = ViewController3()
+
+        pages.append(page1)
+        pages.append(page2)
+        pages.append(page3)
+
+        currentVC = pages.first!
+
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemPurple
+
+        // ✨TIP: "부모" 뷰 컨트롤러에 "자식" 뷰 컨트롤러를 추가함 (아래 3줄!)
+        addChild(pageViewController)
+        view.addSubview(pageViewController.view)
+        pageViewController.didMove(toParent: self)  // ✨ 자식에게도 부모를 추가해줌
+
+        pageViewController.dataSource = self
+        pageViewController.view.translatesAutoresizingMaskIntoConstraints = false
+
+        NSLayoutConstraint.activate([
+            pageViewController.view.topAnchor.constraint(equalTo: view.topAnchor),
+            pageViewController.view.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            pageViewController.view.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            pageViewController.view.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+        ])
+
+        pageViewController.setViewControllers([pages.first!], direction: .forward, animated: false)
+        currentVC = pages.first!
+    }
+
+}
+
+// MARK: - UIPageViewControllerDataSource
+extension OnboardingContainerViewController: UIPageViewControllerDataSource {
+
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        return getPreviousViewController(from: viewController)
+    }
+
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        return getNextViewController(from: viewController)
+    }
+
+    private func getPreviousViewController(from viewController: UIViewController) -> UIViewController? {
+        guard let index = pages.firstIndex(of: viewController), index - 1 >= 0 else { return nil }
+        currentVC = pages[index - 1]
+        return pages[index - 1]
+    }
+
+    private func getNextViewController(from viewController: UIViewController) -> UIViewController? {
+        guard let index = pages.firstIndex(of: viewController), index + 1 < pages.count else { return nil }
+        currentVC = pages[index + 1]
+        return pages[index + 1]
+    }
+
+    func presentationCount(for pageViewController: UIPageViewController) -> Int {
+        return pages.count
+    }
+
+    func presentationIndex(for pageViewController: UIPageViewController) -> Int {
+        return pages.firstIndex(of: self.currentVC) ?? 0
+    }
+
+}
+
+
+// MARK: - ViewControllers
+class ViewController1: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemRed
+    }
+}
+
+class ViewController2: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemGreen
+    }
+}
+
+class ViewController3: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+    }
+}
