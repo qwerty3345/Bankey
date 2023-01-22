4772845e7c76398725c73b442eb482ddd0d8c383
qwerty3345 <qwerty3345@naver.com>
2023-01-22 12:19:24 +0900

    design: 로그인 화면 UI 세팅

--- Bankey/Login/LoginView.swift
+++ Bankey/Login/LoginView.swift
@@ -9,6 +9,32 @@ import UIKit
 
 class LoginView: UIView {
 
+    let stackView: UIStackView = {
+        let stack = UIStackView()
+        stack.axis = .vertical
+        stack.spacing = 8
+        return stack
+    }()
+
+    let usernameTextField: UITextField = {
+        let tf = UITextField()
+        tf.placeholder = "Username"
+        return tf
+    }()
+
+    let passwordTextField: UITextField = {
+        let tf = UITextField()
+        tf.placeholder = "Password"
+        tf.isSecureTextEntry = true
+        return tf
+    }()
+
+    let dividerView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .secondarySystemFill
+        return view
+    }()
+
     override init(frame: CGRect) {
         super.init(frame: frame)
 
@@ -19,20 +45,57 @@ class LoginView: UIView {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-
-    override var intrinsicContentSize: CGSize {
-        return CGSize(width: 200, height: 200)
-    }
 }
 
 extension LoginView {
     func configureStyle() {
+        backgroundColor = .secondarySystemBackground
+        layer.cornerRadius = 5
+        clipsToBounds = true
+
         translatesAutoresizingMaskIntoConstraints = false
-        backgroundColor = .orange
+        usernameTextField.translatesAutoresizingMaskIntoConstraints = false
+        passwordTextField.translatesAutoresizingMaskIntoConstraints = false
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        dividerView.translatesAutoresizingMaskIntoConstraints = false
+
+        usernameTextField.delegate = self
+        passwordTextField.delegate = self
     }
 
     func configureLayout() {
-        
+        stackView.addArrangedSubview(usernameTextField)
+        stackView.addArrangedSubview(dividerView)
+        stackView.addArrangedSubview(passwordTextField)
+        addSubview(stackView)
+
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalToSystemSpacingBelow: topAnchor, multiplier: 1),
+            stackView.leadingAnchor.constraint(equalToSystemSpacingAfter: leadingAnchor, multiplier: 1),
+            trailingAnchor.constraint(equalToSystemSpacingAfter: stackView.trailingAnchor, multiplier: 1),
+            bottomAnchor.constraint(equalToSystemSpacingBelow: stackView.bottomAnchor, multiplier: 1),
+
+            dividerView.heightAnchor.constraint(equalToConstant: 1)
+        ])
     }
 }
 
+extension LoginView: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        usernameTextField.endEditing(true)
+        passwordTextField.endEditing(true)
+        return true
+    }
+
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if textField.text != "" {
+            return true
+        } else {
+            return false
+        }
+    }
+
+    func textFieldDidEndEditing(_ textField: UITextField) {
+
+    }
+}
--- Bankey/Login/LoginViewController.swift
+++ Bankey/Login/LoginViewController.swift
@@ -8,9 +8,29 @@
 import UIKit
 
 class LoginViewController: UIViewController {
+
     // MARK: - Properties
     let loginView = LoginView()
 
+    let signInButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.backgroundColor = .systemBlue
+        button.setTitleColor(.white, for: .normal)
+        button.setTitle("Sign In", for: [])
+        button.addTarget(LoginViewController.self, action: #selector(signInTapped), for: .primaryActionTriggered)
+        button.layer.cornerRadius = 5
+        return button
+    }()
+
+    let errorMessageLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.textColor = .systemRed
+        label.numberOfLines = 0
+        label.text = "Error Failure"
+        label.isHidden = true
+        return label
+    }()
 
     // MARK: - Lifecycle
     override func viewDidLoad() {
@@ -20,8 +40,6 @@ class LoginViewController: UIViewController {
         configureLayout()
     }
 
-    // MARK: - Actions
-
 
     // MARK: - Helpers
 }
@@ -30,16 +48,42 @@ class LoginViewController: UIViewController {
 extension LoginViewController {
     func configureStyle() {
         loginView.translatesAutoresizingMaskIntoConstraints = false
+        signInButton.translatesAutoresizingMaskIntoConstraints = false
+        errorMessageLabel.translatesAutoresizingMaskIntoConstraints = false
     }
 
     func configureLayout() {
         view.addSubview(loginView)
+        view.addSubview(signInButton)
+        view.addSubview(errorMessageLabel)
 
+        // LoginView
         NSLayoutConstraint.activate([
             loginView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-            loginView.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 1),
-            view.trailingAnchor.constraint(equalToSystemSpacingAfter: loginView.trailingAnchor, multiplier: 1)
-            
+            loginView.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 2),
+            view.trailingAnchor.constraint(equalToSystemSpacingAfter: loginView.trailingAnchor, multiplier: 2)
+            ])
+
+        // Button
+        NSLayoutConstraint.activate([
+            signInButton.leadingAnchor.constraint(equalTo: loginView.leadingAnchor),
+            signInButton.trailingAnchor.constraint(equalTo: loginView.trailingAnchor),
+            signInButton.topAnchor.constraint(equalToSystemSpacingBelow: loginView.bottomAnchor, multiplier: 2)
+            ])
+
+        // Error Label
+        NSLayoutConstraint.activate([
+            errorMessageLabel.leadingAnchor.constraint(equalTo: loginView.leadingAnchor),
+            errorMessageLabel.trailingAnchor.constraint(equalTo: loginView.trailingAnchor),
+            errorMessageLabel.topAnchor.constraint(equalToSystemSpacingBelow: signInButton.bottomAnchor, multiplier: 3)
         ])
+
+    }
+}
+
+// MARK: - Actions
+extension LoginViewController {
+    @objc func signInTapped() {
+
     }
 }
